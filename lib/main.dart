744d9ebef2a0e208a584eda79382e5ada7972c21
thadathilsinan <thadathilsinan@gmail.com>
2024-11-13 15:09:43 +0530

    Request notification permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:chat_app/core/router/router.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
@@ -14,7 +16,7 @@ void main() async {
   runApp(const ProviderScope(child: App()));
 }
 
-class App extends StatelessWidget {
+class App extends HookWidget {
   static final navigatorKey = GlobalKey<NavigatorState>();
   static final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
@@ -22,6 +24,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// Request for notification permission if not already allowed
+    useEffect(() {
+      AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+        if (!isAllowed) {
+          AwesomeNotifications().requestPermissionToSendNotifications();
+        }
+      });
+
+      return null;
+    }, []);
+
     return MaterialApp.router(
       routerConfig: router,
       scaffoldMessengerKey: App.scaffoldMessengerKey,
